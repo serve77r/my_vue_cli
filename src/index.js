d15b9c1be21f6dc49b61b39e391d5a4c0b50574b
serve77r <1931130013@qq.com>
2018-08-19 14:05:59 +0800

    添加注释

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 // const img1 = require('./asset/s.jpg');
 // const resetCss = require('./asset/index.css');
 const Vue = require('vue');
-const Main = require('./Main').default;
-// console.log(Main);
-// Vue.component('Ma',Main);
+const Main = require('./Main').default;//需要添加default,
+// 参考https://github.com/vuejs/vue-loader/releases/tag/v13.0.0
+
 new Vue({
   el: '#main',
   components:{
     Main
   },
-  // render: (createElement) => createElement(Main),
-  // template:'<div class="example">{{ msg }}</div>'
   template:'<Main/>',
-  // render: h => h(Main)
 })
-
-// console.log('可以了');
